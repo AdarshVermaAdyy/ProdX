9d97c2e28aec8e68c58012a7faaef4b16c53ff0f
Adarsh Verma <adarsh.verma1@in.ey.com>
2024-08-30 13:14:02 +0530

    dev branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ ];
---
+++ src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {  ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav'
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatBadgeModule} from '@angular/material/badge';
+import { MatInputModule } from '@angular/material/input';
+
+
+@Component({
+  selector: 'app-sidenav',
+  standalone: true,
+  imports: [ MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatInputModule,
+    MatListModule,CommonModule,MatFormFieldModule],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss'
+})
+export class SidenavComponent {
+  isExpanded = true;
+  showSubmenu: boolean = false;
+  isShowing = false;
+  showSubSubMenu: boolean = false;
+
+  mouseenter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+
+  mouseleave() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
+  @ViewChild('sidenav') sidenav:any= MatSidenav;
+
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
+}
