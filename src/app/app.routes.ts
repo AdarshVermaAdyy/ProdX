780fe43256b988bbe941264400a3db1ab32fca90
Sanal Jain <sanal.jain@in.ey.com>
2024-09-06 09:22:02 +0530

    login and register pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
    
     {
-        path:'login',
+        path:'auth',
         loadChildren : ()=>import('./Auth/auth.module').then(m=>m.AuthModule)
     },
     {
